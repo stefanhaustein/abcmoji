e367a0ec135da8a726e5066db1928381d8a498df
Stefan Haustein <stefan.haustein@gmail.com>
2019-08-25 16:36:59 +0200

    abc notation handling improvments

--- core/src/main/java/org/kobjects/sound/Sound.java
+++ core/src/main/java/org/kobjects/sound/Sound.java
@@ -1,82 +1,280 @@
 package org.kobjects.sound;
 
 
+import org.kobjects.atg.ToneGenerator;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+// TODO: It will make sense to factor out play state into an inner class, in particular with multiple
 public class Sound {
 
-    String s;
-    int pos;
-    SampleManager manager;
+  final SampleManager manager;
+  float baseLength = 1f/8f;
+  boolean explicitLength = false;
+  ToneGenerator toneGenerator = new ToneGenerator();
+  String[] lines;
+  String line;
+  int lineNumber;
+  int pos;
+  int len;
+  float dividend;
+  float divisor;
+  float bpmMultiplier = 1f/4f;
+  float bpm = 120/4;
 
-    public Sound(SampleManager manager, String notes) {
-        this.manager = manager;
-        this.s = notes;
+  public Sound(SampleManager manager, String abcData) {
+    this.manager = manager;
+    lines = abcData.split("\n");
+  }
+
+
+  public void play() {
+    play(false);
+  }
+
+  public void play(boolean blocking) {
+    if (blocking) {
+      try {
+        playImpl();
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    } else {
+      new Thread(() -> play(true)).start();
+    }
+  }
+
+  private void playImpl() throws InterruptedException {
+    for (lineNumber = 0; lineNumber < lines.length; lineNumber++) {
+      line = lines[lineNumber];
+      len = line.length();
+      pos = 0;
+      if (len > 1 && line.charAt(0) >= 'A' && line.charAt(1) <= 'Z' && line.charAt(1) == ':') {
+        pos += 2;
+        processCommand(line.charAt(0), line.substring(2).trim());
+      } else {
+        boolean nowait = false;
+        int count = 0;
+        int firstTime = 0;
+        while (pos < len && line.charAt(pos) != '%') {
+          switch (line.charAt(pos)) {
+            case '[':
+              nowait = true;
+              count = 0;
+              pos++;
+              continue;
+            case ']':
+              nowait = false;
+              if (count > 1){
+                Thread.sleep(firstTime);
+              }
+              pos++;
+              continue;
+          }
+          int time = processNote(nowait);
+          if (count == 0) {
+            firstTime = time;
+          }
+          count++;
+        }
+      }
+    }
+  }
+
+
+  private float parseNumber() {
+    dividend = 0;
+    int p0 = pos;
+    while (pos < len && line.charAt(pos) >= '0' && line.charAt(pos) <= '9') {
+      dividend = dividend * 10 + (line.charAt(pos++) - '0');
+    }
+    if (pos == p0) {
+      dividend = 1;
+    }
+    if (pos >= len || line.charAt(pos) != '/') {
+      divisor = 1;
+    } else {
+      divisor = 0;
+      pos++;
+      int p1 = pos;
+      while (pos < len && line.charAt(pos) >= '0' && line.charAt(pos) <= '9') {
+        divisor = divisor * 10 + (line.charAt(pos++) - '0');
+      }
+      if (p1 == p0) {
+        divisor = 2;
+      }
+    }
+    return dividend/divisor;
+  }
+
+  void skipSpace() {
+    while (pos < len && line.charAt(pos) == ' ') {
+      pos++;
+    }
+  }
+
+  private void parseTempo() {
+    skipSpace();
+    float beforeEq = 0;
+    boolean eqFound = false;
+    while (pos < len) {
+      if (line.charAt(pos) == '=') {
+        eqFound = true;
+        break;
+      }
+      beforeEq += parseNumber();
+      skipSpace();
+    }
+    if (eqFound) {
+      bpmMultiplier = beforeEq;
+      skipSpace();
+      bpm = parseNumber() * bpmMultiplier;
+    } else {
+      bpm = beforeEq * bpmMultiplier;
+    }
+  }
+
+  private void processCommand(char command, String data) {
+    switch (command) {
+      case 'L':
+        baseLength = parseNumber();
+        explicitLength = true;
+        System.out.println("base length: " + baseLength);
+        break;
+      case 'M':
+        float meter = data.equals("C") || data.equals("C|") ? 1 : parseNumber();
+        if (!explicitLength && meter < 0.75) {
+          baseLength = 1f/16f;
+        }
+        bpmMultiplier = 1f/divisor;
+        explicitLength = true; // prevent changes by M in body.
+        break;
+      case 'Q':
+        parseTempo();
+        break;
+
+      default:
+        System.out.println("Ignored command: " + line);
+    }
+  }
+
+  private int processNote(boolean nowait) throws InterruptedException {
+    float note = 0;
+    while (pos < len) {
+      switch (line.charAt(pos)) {
+        case '_':
+          note--;
+          pos++;
+          continue;
+        case '^':
+          note++;
+          pos++;
+          continue;
+      }
+      break;
     }
 
-    public void play() {
-        play(false);
+    if (pos >= len) {
+      return 0;
     }
 
-    void play(boolean mayBlock) {
-        while (pos < s.length()) {
-            final int codePoint = Character.codePointAt(s, pos);
-            pos += Character.charCount(codePoint);
-            final double f;
-            switch (codePoint) {
-                case 'C': f = 261.63; break;
-                case 'D': f = 293.66; break;
-                case 'E': f = 329.63; break;
-                case 'F': f = 349.23; break;
-                case 'G': f = 392; break;
-                case 'A': f = 440; break;
-                case 'B': f = 493.88; break;
-                case 'c': f = 523.25; break;
-                case 'd': f = 587.33; break;
-                case 'e': f = 659.26; break;
-                case 'f': f = 698.46; break;
-                case 'g': f = 783.99; break;
-                case 'a': f = 880; break;
-                case 'b': f = 987.77; break;
-                default:
-                    manager.play(Integer.toHexString(codePoint), new Runnable() {
-                        public void run() {
-                            play(false);
-                        }
-
-                    });
-                    return;
-            }
-
-            double duration = 0.25;
-            if (pos < s.length()) {
-                char c = s.charAt(pos);
-                boolean divide = false;
-                if (c == '/') {
-                    pos++;
-                    divide = true;
-                }
-                int len = 0;
-                while (pos < s.length() && s.charAt(pos) >= '0' && s.charAt(pos) <= '9') {
-                    len = len * 10 + (s.charAt(pos++) - '0');
-                }
-                if (divide) {
-                    duration /= len == 0 ? 2 : len;
-                } else if (len != 0) {
-                    duration *= len;
-                }
-            }
-            if (mayBlock) {
-                Tone.play(f, duration);
-            } else {
-                final double finalDuration = duration;
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Tone.play(f, finalDuration);
-                        play(true);
-                    }
-                }).start();
-                return;
-            }
+    final int codePoint = Character.codePointAt(line, pos);
+    pos += Character.charCount(codePoint);
+    boolean rest = false;
+    switch (codePoint) {
+      case 'C':
+        note -= 9;
+        break;
+      // C#
+      case 'D':
+        note -= 7;
+        break;
+      // D#
+      case 'E':
+        note -= 5;
+        break;
+      case 'F':
+        note -= 4;
+        break;
+      // F#
+      case 'G':
+        note -= 2;
+        break;
+      // G#
+      case 'A':
+        note += 0;
+        break;  // 440Hz
+      // A#
+      case 'B':
+        note += 2;
+        break;
+      case 'c':
+        note += 3;
+        break;
+      // c#
+      case 'd':
+        note += 5;
+        break;
+      // d#
+      case 'e':
+        note += 7;
+        break;
+      case 'f':
+        note += 8;
+        break;
+      // f#
+      case 'g':
+        note += 10;
+        break;
+      // g#
+      case 'a':
+        note += 12;
+        break;
+      // a#
+      case 'b':
+        note += 14;
+        break;
+      case 'x':
+      case 'z':
+        rest = true;
+        break;
+      default:
+        synchronized (this) {
+          BlockingQueue<String> queue = new LinkedBlockingQueue<>();
+          manager.play(
+              Integer.toHexString(codePoint),
+              () -> {
+                queue.add("done");
+            });
+          queue.take();
+          return 0;
         }
     }
+    while (pos < len) {
+      switch (line.charAt(pos)) {
+        case '\'':
+          note += 12;
+          pos++;
+          continue;
+        case ',':
+          note -= 12;
+          pos++;
+          continue;
+      }
+      break;
+    }
+
+    float duration = baseLength * parseNumber() * 60 / bpm;
+    float frequency = (float) (440 * Math.pow(2, note/12f));
+
+    int durationMs = (int) (duration*1000);
+    if (!rest) {
+      toneGenerator.play(frequency, durationMs);
+    }
+    if (!nowait) {
+      Thread.sleep(durationMs);
+    }
+    return durationMs;
+  }
 }
--- demo/src/main/java/org/kobjects/graphics/demo/MainActivity.java
+++ demo/src/main/java/org/kobjects/graphics/demo/MainActivity.java
@@ -2,6 +2,7 @@ package org.kobjects.graphics.demo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -22,13 +23,24 @@ public class MainActivity extends Activity {
     LinearLayout layout = new LinearLayout(this);
     layout.setOrientation(LinearLayout.VERTICAL);
     EditText editText = new EditText(this);
-    layout.addView(editText);
+    layout.addView(editText, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 0, 1));
+    editText.setText(
+        "% Generated more or less automatically by swtoabc by Erich Rickheit KSC\n"
+    + "X:1\n"
+    + "T:Frere Jacques\n"
+    + "M:4/4\n"
+    + "L:1/4\n"
+    + "K:C\n"
+    + "c d e c| c d e c| e f g2| e f g2| g/2a/2 g/2f/2 e c| g/2a/2 g/2f/2 e c|c G c2| c G c2|\n");
+
 
     Button button = new Button(this);
     button.setText("Play notes");
     layout.addView(button);
 
-    button.setOnClickListener(view -> new Sound(sampleManager, editText.getText().toString()).play());
+    button.setOnClickListener(view -> {
+      new Sound(sampleManager, editText.getText().toString()).play();
+    });
 
     setContentView(layout);
   }
